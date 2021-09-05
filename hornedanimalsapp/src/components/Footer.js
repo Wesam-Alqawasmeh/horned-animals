64c4047b948815e442b5558954c37319913db69f
Wesam-Alqawasmeh <alqawasmehwesam@gmail.com>
2021-09-05 21:43:00 +0300

    lab 01

---
+++ hornedanimalsapp/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <>
+              <p>All Right Reserved 2021 &copy; Wesam Alqawasmeh</p> 
+            </>
+        )
+    }
+}
+
+export default Footer
