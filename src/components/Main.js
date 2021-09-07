faf8380e7523f59c3099124111216e14b501dcc7
Wesam-Alqawasmeh <alqawasmehwesam@gmail.com>
2021-09-07 16:28:02 +0300

    lab 03

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,17 @@
 import React, { Component } from "react";
 import HornedBeasts from "./HornedBeasts";
-import data from "../data.json";
-import { Row } from "react-bootstrap";
+
 
 class Main extends Component {
   render() {
     return (
       <>
-        <Row>
-          {data.map((item) => {
-            return (
-              <HornedBeasts
-                title={item.title}
-                imageUrl={item.image_url}
-                description={item.description}
-              />
-            );
-          })}
-        </Row>
+        <HornedBeasts
+          title={this.props.title}
+          imageUrl={this.props.imageUrl}
+          description={this.props.description}
+          handleOpen={this.props.handleOpen}
+        />
       </>
     );
   }
